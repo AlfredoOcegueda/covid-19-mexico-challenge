558ccd99bb7b31c453973631ad7b427764a2f25d
Pamela Hernandez <pamelahernandez@Pamelas-MacBook-Pro.local>
2020-11-30 15:42:02 -0800

    update my branch

--- StarterCode copy/app.js
+++ StarterCode copy/app.js
@@ -18,9 +18,7 @@ function buildData(data) {
 function buildCharts(){
   d3.csv("data.csv").then((data) => {
     var dates = data.Dates;
-    console.log(dates);
     var positives = data.Positives;
-    console.log(positives);
     var states = data.State;
     console.log(states);
 
@@ -41,6 +39,61 @@ function buildCharts(){
 
     Plotly.newPlot('bar', scatterData, layout);
   });
+
+    var barchart = [{
+        type: 'bar',
+        x: [20, 14, 23],
+        y: ['giraffes', 'orangutans', 'monkeys'],
+        orientation: 'h'
+    }];
+  
+    Plotly.newPlot('gauge', barchart); 
+
+    Plotly.d3.csv('data2.csv', function(err, rows){
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+      }
+
+      var data = [{
+          type: 'choropleth',
+          locationmode: 'MEXICO-states',
+          locations: unpack(rows, 'code'),
+          z: unpack(rows, 'Postives'),
+          text: unpack(rows, 'States'),
+          zmin: 0,
+          zmax: 17000,
+          colorscale: [
+              [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
+              [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
+              [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
+          ],
+          colorbar: {
+              title: '# cases',
+              thickness: 0.2
+          },
+          marker: {
+              line:{
+                  color: 'rgb(255,255,255)',
+                  width: 2
+              }
+          }
+      }];
+
+
+      var layout = {
+          title: 'COVID 19 cases by State',
+          geo:{
+              scope: 'mexico',
+              showlakes: true,
+              lakecolor: 'rgb(255,255,255)'
+          }
+      };
+
+      Plotly.newPlot("bubble", data, layout, {showLink: false});
+});
+
+
+
 }
 
 function initFunction(){
@@ -50,7 +103,7 @@ function initFunction(){
         Object.entries(data).forEach(([index,value]) => {
             selection.append("option").text(value);
         })
-        buildCharts(data[0]);
+        buildCharts(data);
         buildData(data[0]);
     });
 }
