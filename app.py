715dfec52417ac3c46a41c906b68af17d98f63d7
Pamela Hernandez <pamelahernandez@Pamelas-MacBook-Pro.local>
2020-11-30 20:22:29 -0800

    to my branch

--- app.py
+++ app.py
@@ -14,10 +14,12 @@ States = Base.classes.states_info
 #Ages = Base.classes.ages_distribution
 #Setup of Flask
 app = Flask(__name__)
+
 @app.route("/")
 def index():
     #Mars = mongo.db.Mars.find_one()
     return render_template("index.html", states = states)
+
 @app.route("/mexico")
 def mexico():
     # Visualize confirmed cases in a heatmap of the country
@@ -29,16 +31,13 @@ def mexico():
     confirmed_cases = list(np.ravel(results))
     print(confirmed_cases)
     return render_template("mexico.html", confirmed = confirmed_cases)
+
 @app.route("/states")
 def states():
     session = Session(engine)
     results = session.query(Covid.State_ID, States.State_Name, Covid.Confirmed, Covid.Negatives, Covid.Suspicious, Covid.Deaths, States.Latitude, States.Longitude).filter((Covid.State_ID == States.State_ID) & (Covid.State_ID == 1))
     session.close()
     print(results)
-<<<<<<< HEAD
-=======
-
->>>>>>> 4fc7a36786f7d5ccc0f5416501c26c1e35ab5cb0
     state2 = []
     for state, statename, confirmed, negatives, suspicious, deaths, latitude, longitude in results:
         state_dict = {}
@@ -51,14 +50,10 @@ def states():
         state_dict["latitude"] = latitude
         state_dict["longitude"] = longitude
         state2.append(state_dict)
-<<<<<<< HEAD
-    return render_template("states.html", states=state)
-=======
 
 
     return render_template("states.html", states=state)
 
->>>>>>> 4fc7a36786f7d5ccc0f5416501c26c1e35ab5cb0
 @app.route("/states/<state_id>")
 def states_info(state_id):
     session = Session(engine)
@@ -80,20 +75,13 @@ def states_info(state_id):
     print(filtered_states)
     #jsonify(all_states)    
     return jsonify (filtered_states)
-<<<<<<< HEAD
-=======
 
->>>>>>> 4fc7a36786f7d5ccc0f5416501c26c1e35ab5cb0
 @app.route("/states/all")
 def states_all():
     session = Session(engine)
     results = session.query(Covid.State_ID, States.State_Name, Covid.Confirmed, Covid.Negatives, Covid.Suspicious, Covid.Deaths, States.Latitude, States.Longitude).all()
     session.close()
-    print(results)
-<<<<<<< HEAD
-=======
-
->>>>>>> 4fc7a36786f7d5ccc0f5416501c26c1e35ab5cb0
+    #print(results)
     all_states = []
     for state, statename, confirmed, negatives, suspicious, deaths, latitude, longitude in results:
         state_dict = {}
@@ -105,19 +93,10 @@ def states_all():
         state_dict["deaths"] = deaths
         state_dict["latitude"] = latitude
         state_dict["longitude"] = longitude
-        all_states.append(state_dict)
-<<<<<<< HEAD
-    print(all_states)
-    #jsonify(all_states)    
-    return jsonify (all_states)
-=======
-
-    print(all_states)
-    #jsonify(all_states)    
+        all_states.append(state_dict) 
 
     return jsonify (all_states)
 
->>>>>>> 4fc7a36786f7d5ccc0f5416501c26c1e35ab5cb0
 @app.route("/comparison")
 def comparison():
     return render_template("comparison.html")
